05ff42b0c1f5a5445ad094cb6dc05065640b6af2
Anass Al <mr@anassinator.com>
2016-05-15 20:23:41 -0700

    Add web-image-view electron application

---
+++ app.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const {app, BrowserWindow, globalShortcut} = require("electron");
+
+// Set up command-line arguments.
+const yargs = require("yargs")
+  .usage("usage: $0 TOPIC [--server addr] [-w num] [-h num] [-q num]")
+  .demand(1)
+  .default("server", "http://ROS_MASTER_URI:8080/")
+  .describe("server", "Video server address")
+  .default("w", 400).alias("w", "width").describe("w", "Image width")
+  .default("h", 300).alias("h", "height").describe("h", "Image height")
+  .default("q", 30).alias("q", "quality")
+  .describe("q", "Image quality (0-100)")
+const argv = process.platform === "darwin" ?
+             yargs.argv :
+             yargs.parse(process.argv.splice(1));
+
+// Keep a global reference of the window object to save it from the garbage
+// collector.
+let mainWindow;
+
+app.on("ready", () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: argv.w,
+    height: argv.h,
+    darkTheme: true,
+    icon: path.join(__dirname, "/resources/ros.icns")
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+  // Send command-line arguments to renderer.
+  let target = {
+    "host": argv.server,
+    "topic": argv._[0],
+    "quality": argv.q
+  };
+  mainWindow.webContents.on("did-finish-load", () => {
+    mainWindow.webContents.send("set-target", target);
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", function () {
+    // Dereference the window object.
+    mainWindow = null;
+  });
+});
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function () {
+  app.quit();
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ renderer.js
@@ -0,0 +1,187 @@
+const remote = require("electron").remote;
+const ipcRenderer = require("electron").ipcRenderer;
+
+let stream, download_lnk;
+let hostname, topic, quality;
+let win = remote.getCurrentWindow();
+
+// Remove all previous window listeners, to fix issues when refreshing.
+win.removeAllListeners();
+
+function setHostname(url) {
+  // Sets hostname.
+  if (url.endsWith("/")) {
+    hostname = url;
+  } else {
+    hostname = url + "/";
+  }
+}
+
+function setTopic(name) {
+  // Sets topic name.
+  topic = name;
+}
+
+function setQuality(q) {
+  // Sets image quality.
+  quality = q;
+}
+
+function setTitle() {
+  // Sets window title.
+  document.title = topic + " (" + quality + "%)";
+}
+
+function play() {
+  // Remove event listener to avoid firing on every frame of stream.
+  stream.removeEventListener("load", play, false);
+
+  // Set opaque.
+  stream.style.opacity = 1.0;
+
+  // Fix stream on resize.
+  updateStream();
+  window.addEventListener("resize", updateStream, false);
+
+  // Set up quality shortcuts.
+  document.onkeypress = onKeyPress;
+  document.oncontextmenu = screenshot;
+}
+
+function getAspectRatio() {
+  // Remove event listener to avoid firing on every frame of stream.
+  stream.removeEventListener("load", getAspectRatio, false);
+
+  // Keep raw dimensions of frame.
+  raw_aspect_ratio = stream.width / stream.height;
+
+  // Set window size and aspect ratio.
+  win.setAspectRatio(raw_aspect_ratio);
+  win.setContentSize(Math.round(window.innerHeight * raw_aspect_ratio),
+                     window.innerHeight, true);
+
+  // Set stream as visible on stream startup.
+  stream.addEventListener("load", play, false);
+
+  // Set up stream with proper dimensions.
+  updateStream();
+}
+
+function updateStream() {
+  // Get current window dimensions.
+  width = window.innerWidth;
+  height = window.innerHeight;
+  if (width === 0 || height === 0) {
+    return;
+  }
+  aspect_ratio = width / height;
+
+  // Determine limiting dimension, and maintain aspect ratio.
+  if (aspect_ratio > raw_aspect_ratio) {
+    width = Math.round(height * raw_aspect_ratio);
+  } else {
+    height = Math.round(width / raw_aspect_ratio);
+  }
+
+  // Set dimensions.
+  stream.width = width;
+  stream.height = height;
+
+  // Stream.
+  stream.src = hostname + "stream?topic=" + topic +
+               "&width=" + width + "&height=" + height +
+               "&quality=" + quality;
+}
+
+function onKeyPress(e) {
+  // Set quality.
+  switch (String.fromCharCode(e.keyCode)) {
+    case "1":
+      setQuality(10);
+      break;
+    case "2":
+      setQuality(20);
+      break;
+    case "3":
+      setQuality(30);
+      break;
+    case "4":
+      setQuality(40);
+      break;
+    case "5":
+      setQuality(50);
+      break;
+    case "6":
+      setQuality(60);
+      break;
+    case "7":
+      setQuality(70);
+      break;
+    case "8":
+      setQuality(80);
+      break;
+    case "9":
+      setQuality(90);
+      break;
+    case "0":
+      setQuality(100);
+      break;
+    case "+":
+      setQuality(Math.min(100, quality + 5));
+      break;
+    case "-":
+      setQuality(Math.max(0, quality - 5));
+      break;
+    default:
+      return;
+  }
+  
+  // Update stream and title.
+  setTitle();
+  updateStream();
+}
+
+function screenshot(e) {
+  // Prevent context menu from appearing.
+  e.preventDefault();
+
+  // Create canvas.
+  let canvas = document.createElement("canvas");
+  canvas.width = stream.width;
+  canvas.height = stream.height;
+
+  // Draw image to canvas.
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(stream, 0, 0);
+
+  // Get image data as PNG and set up download link
+  let imageData = canvas.toDataURL("image/png");
+  download_lnk.href = imageData.replace(/^data:image\/png/,
+                                    "data:application/octet-stream");
+  download_lnk.download = new Date().toJSON() + ".png";
+
+  // Download.
+  download_lnk.click();
+}
+
+window.addEventListener("load", () => {
+  // Find items on page.
+  stream = document.getElementById("stream");
+  download_lnk = document.getElementById("download");
+
+  // Hide stream until ready.
+  stream.style.opacity = 0;
+
+  // Get command-line arguments.
+  ipcRenderer.on("set-target", (event, message) => {
+    // Set properties.
+    setHostname(message["host"]);
+    setTopic(message["topic"]);
+    setQuality(message["quality"]);
+    setTitle();
+
+    // Get a full-size low quality snapshot to determine aspect ratio.
+    stream.src = hostname + "snapshot?topic=" + topic + "&quality=0";
+    stream.addEventListener("load", getAspectRatio, false);
+  });
+}, false);
