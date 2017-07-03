f709fbe9e9aa20d84a9ea17a9d2e60642e23bcf3
Anass Al <mr@anassinator.com>
2017-07-02 19:08:38 -0700

    Package using electron-packager and build in devel

---
+++ package.js
@@ -0,0 +1,16 @@
+const packager = require('electron-packager');
+
+packager({
+  dir: '.',
+  name: 'web_image_view',
+  appCopyright: 'McGill Robotics',
+  icon: './src/resources/icons/ros.ico',
+  prune: false,
+  quiet: true,
+  out: './release',
+  overwrite: true,
+}, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
